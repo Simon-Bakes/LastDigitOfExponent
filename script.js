02ff02ed833e654538310219e583fef1b2fd3fd5
Simon-Bakes <155891368+Simon-Bakes@users.noreply.github.com>
2024-01-21 19:03:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,96 +5,97 @@ function power(){
 
     let output = "";
 
-    if(y==0){
-           output+="1"; 
-    }else if(x==1){
-      switch(1%y){
-        default:
-            output+="1"; 
-      } 
-    }else if(x==2){
-      switch(Math.pow(x, y)%10){
-        case 2:     
-            output+="2";
-            break;
-        case 4:
-            output+="4";
-            break;
-        case 6:
+    if(x==0){
+           output+="0"; 
+    }else if(x==1 || x==5 || x==6){
+            output+=x;
+    }else{
+      switch(x){
+        case 2:
+      switch(y%4){
+        case 0:
             output+="6";
             break;
-        case 8:
+        case 1:
+            output+="2";
+            break;
+        case 2:
+            output+="4";
+            break;
+        case 3:
             output+="8";
             break;
       }
-    }else if(x==3){
-      switch(Math.pow(x, y)%10){
-        case 1:
+      break;
+    case 3:
+      switch(y%4){
+        case 0:
             output+="1";
             break;
-        case 3:
+        case 1:
             output+="3";
             break;
-        case 7:
-            output+="7";
-            break;
-        case 9:
+        case 2:
             output+="9";
             break;
+        case 3:
+            output+="7";
+            break;
       }
-    }else if(x==4){
-      switch(Math.pow(x, y)%10){
-        case 4:
+      break;
+    case 4:
+      switch(y%2){
+        case 0:
+            output+="6";
+            break;
+        case 1:
             output+="4";
-            break;
-        case 6:
-            output+="6";
             break;  
-      }
-    }else if(x==5){
-      switch(y){
-        default:
-            output+="5"; 
-      }      
-    }else if(x==6){
-       switch(y){
-        default:
-            output+="6";
-      }       
-    }else if(x==7){
-      switch(Math.pow(x, y)%10){
-        case 3:
-            output+="3";
+      }   
+      break;     
+    case 7:
+      switch(y%4){
+        case 0:
+            output+="1";
             break;
-        case 7:
+        case 1:
             output+="7";
             break;
-        case 9:
+        case 2:
             output+="9";
+            break;
+        case 3:
+            output+="3";
             break;
       }  
-    }else if(x==8){
-     switch(Math.pow(x, y)%10){
-        case 2:
-            output+="2";
-            break; 
-        case 4:
-            output+="4";
-            break; 
-        case 6:
+      break;
+    case 8:
+     switch(y%4){
+        case 0:
             output+="6";
             break; 
-        case 8:
+        case 1:
             output+="8";
             break; 
-      }   
-    }else if(x==9){
-      switch(y){
-        default:
+        case 2:
+            output+="4";
+            break; 
+        case 3:
+            output+="2";
+            break; 
+      } 
+      break;  
+    case 9:{
+      switch(y%2){
+        case 0:
+            output+="1";
+            break; 
+        case 1:
             output+="9";
-      }
-    }else if(x==0){
-      output+="0";
+            break; 
+      }}
+      break;
     }
+      }
     document.getElementById("output").innerHTML = "The last digit of "+z+"<sup>"+y+"</sup> is: " +output;
-}
+    }
